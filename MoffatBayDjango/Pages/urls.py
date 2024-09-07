65e5fb9eb38b37e907ad7459b52aecda99e6bd03
Mommer023 <taylormommer@icloud.com>
2024-09-06 21:12:05 -0500

    Pushing Updates -  Taylor
    Views, URLS, Templates and routing

--- MoffatBayDjango/Pages/urls.py
+++ MoffatBayDjango/Pages/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import HomePageView #, AboutPageView **this is an example comment** to actually implement this the about view need to be uncommented and imported here. Then the url needs to be uncommented.
+from .views import HomePageView, AboutPageView, AttractionsPageView, BookPageView, ContactPageView, LoginPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('attractions/', AttractionsPageView.as_view(), name='attractions'),
+    path('book/', BookPageView.as_view(), name='book'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('login/', LoginPageView.as_view(), name='login'),
+
     # path('about/', AboutPageView.as_view(), name='about'),
 ]
--- MoffatBayDjango/Pages/views.py
+++ MoffatBayDjango/Pages/views.py
@@ -6,8 +6,21 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-def navBar(request):
-    return render(request, 'navbar.html')
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class AttractionsPageView(TemplateView):
+    template_name = 'attractions.html'
+
+class BookPageView(TemplateView):
+    template_name = 'book.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+class LoginPageView(TemplateView):
+    template_name = 'login.html'
+
 # class AboutPageView(TemplateView):
 #
 #     template_name = 'about.html'
