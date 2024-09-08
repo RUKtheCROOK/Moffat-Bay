f9db209f4ea0b14a30fee1da65c34cca363efc3c
RUKtheCROOK <johngarcia053@gmail.com>
2024-09-08 12:50:31 -0700

    added use of the django admin panel

--- MoffatBayDjango/Accounts/models.py
+++ MoffatBayDjango/Accounts/models.py
@@ -13,13 +13,21 @@ class CustomUser(AbstractUser):
     password_salt = models.CharField(max_length=128, db_column='Salt', blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now, db_column='CreatedAt')
     last_login = models.DateTimeField(default=timezone.now, db_column='lastLogin')
-    # fields I am excluding from the user model
-    is_superuser = None
-    is_staff = None
-    is_active = None
-    date_joined = None
-    username = None
-    password = None
+
+    # fields I was going to excuse, but decided to use so I can use the admin panel
+    is_superuser = models.BooleanField(default=False, db_column='IsSuperUser')
+    is_staff = models.BooleanField(default=False, db_column='IsStaff')
+    is_active = models.BooleanField(default=True, db_column='IsActive')
+    date_joined = models.DateTimeField(default=timezone.now, db_column='DateJoined')
+    # username = models.CharField(max_length=150, db_column='Username', default='default')
+    # password = models.CharField(max_length=128, db_column='Password', blank=True, null=True)
+
+    # is_superuser = None
+    # is_staff = None
+    # is_active = None
+    # date_joined = None
+    # username = None
+    # password = None
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', 'password_hash', 'password_salt']
