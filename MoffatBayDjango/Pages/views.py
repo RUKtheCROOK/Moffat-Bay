de1a0ee124181a8bcd4da7f0fd684e8e7a3a4beb
RUKtheCROOK <johngarcia053@gmail.com>
2024-09-08 13:21:40 -0700

    Added the active pages

--- MoffatBayDjango/Pages/views.py
+++ MoffatBayDjango/Pages/views.py
@@ -6,17 +6,42 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'home'
+        return context
+
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'about'
+        return context
 
 class AttractionsPageView(TemplateView):
     template_name = 'attractions.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'attractions'
+        return context
+
 class BookPageView(TemplateView):
     template_name = 'book.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'book'
+        return context
+
 class ContactPageView(TemplateView):
     template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'contact'
+        return context
 
 # class AboutPageView(TemplateView):
 #
