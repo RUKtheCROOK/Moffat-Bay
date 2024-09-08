c4ecbe0d9b41de26d01b396f1bf0a4aa9392864b
RUKtheCROOK <johngarcia053@gmail.com>
2024-09-08 16:17:46 -0700

    Added Mod 6 Folder
    Added mod 6 folder and all required mod 6 deliverables

---
+++ Old Assignment Folder/Module 6/MoffatBayDjango/Pages/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'home'
+        return context
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'about'
+        return context
+
+class AttractionsPageView(TemplateView):
+    template_name = 'attractions.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'attractions'
+        return context
+
+class BookPageView(TemplateView):
+    template_name = 'book.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'book'
+        return context
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_type'] = 'contact'
+        return context
+
+# class AboutPageView(TemplateView):
+#
+#     template_name = 'about.html'
